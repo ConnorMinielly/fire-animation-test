e270c9917a36ee66ae4ac7de8e0162315b40b544
ConnorMinielly <Connor_Minielly@Outlook.com>
2018-09-04 19:16:20 -0400

    playing with headers for security

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       options: {
         headers: {
           '/*': [
-            "Content-Security-Policy: default-src 'self'; script-src 'self';",
+            "Content-Security-Policy: default-src 'self'; script-src 'self'; style-src 'self';",
           ],
         },
       },
