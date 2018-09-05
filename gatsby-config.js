5d57112557d91cd1936eaa2bb0a15516aaec76e1
ConnorMinielly <Connor_Minielly@Outlook.com>
2018-09-05 13:06:42 -0400

    playing with headers for security

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
       resolve: 'gatsby-plugin-netlify',
       options: {
         headers: {
-          '/*': ['Content-Security-Policy: default-src https: ;'],
+          '/*': ['Content-Security-Policy: default-src https:*;'],
         },
       },
     },
